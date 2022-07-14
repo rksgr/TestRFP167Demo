d419686ee944dfdb6d45a4b265406941c331bd9a
Rahul <r_kjha@yahoo.com>
2022-07-14 13:39:14 +0530

    Added: Welcome message

---
+++ src/Welcome.java
@@ -0,0 +1,10 @@
+public class Welcome {
+    public void display(){
+        System.out.println("Welcome to BridgeLabz!");
+    }
+
+    public static void main(String[] args) {
+        Welcome we = new Welcome();
+        we.display();
+    }
+}
